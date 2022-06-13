0d36f62f81ac3748078ee2e890a0aecd1492c6d7
himprover <hwang.improver@gmail.com>
2022-06-13 11:05:56 +0900

    feat: add facebook login [MINFOLINK-6]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import type {AppProps} from 'next/app';
+import {SessionProvider} from 'next-auth/react';
 import {ThemeProvider} from 'styled-components';
 import {GlobalStyle} from 'styles/global-styles';
 import {lightTheme} from 'styles/theme';
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
-import {ExampleComponent} from 'components/Name';
 import type {NextPage} from 'next';
+import {signIn, signOut, useSession} from 'next-auth/react';
 
 const Home: NextPage = () => {
-  return <ExampleComponent />;
+  const {data, status} = useSession();
+  return (
+    <div>
+      <button type="button" onClick={() => signIn()}>
+        SignIn
+      </button>
+      <button type="button" onClick={() => signOut()}>
+        SignOut
+      </button>
+      <h1>{data}</h1>
+      <h3>{status}</h3>
+    </div>
+  );
 };
 
 export default Home;
