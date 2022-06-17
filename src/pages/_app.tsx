ddc1a80315c87661b557566c5739fb3b56f7f6f4
himprover <hwang.improver@gmail.com>
2022-06-17 17:41:09 +0900

    feat: add SSR session check to header [MINFOLINK-6]
    1. CSR로 깜빡 거리는 헤더가 아닌 SSR로 깜빡거리지 않게 함

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,32 @@
-import type {AppProps} from 'next/app';
-import {SessionProvider} from 'next-auth/react';
+import type {AppContext, AppProps} from 'next/app';
+import {getSession, SessionProvider} from 'next-auth/react';
 import {ThemeProvider} from 'styled-components';
 import {GlobalStyle} from 'styles/global-styles';
 import {lightTheme} from 'styles/theme';
+import App from 'next/app';
+import {Header} from 'components/common/Header';
+import {Session} from 'next-auth';
 
-function MyApp({Component, pageProps}: AppProps) {
+interface CustomAppProps extends AppProps {
+  session: Session | null;
+}
+
+function MyApp({Component, pageProps, session}: CustomAppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider theme={lightTheme}>
         <GlobalStyle />
+        <Header session={session} />
         <Component {...pageProps} />
       </ThemeProvider>
     </SessionProvider>
   );
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const session = await getSession(appContext.ctx);
+  const appProps = await App.getInitialProps(appContext);
+  return {...appProps, session: session};
+};
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,7 @@
 import type {NextPage} from 'next';
-import {signIn, signOut, useSession} from 'next-auth/react';
-import {useEffect} from 'react';
 
 const Home: NextPage = () => {
-  const {data, status} = useSession();
-  useEffect(() => {}, [data]);
-  return (
-    <div>
-      <button type="button" onClick={() => signIn('facebook')}>
-        SignIn
-      </button>
-      <button type="button" onClick={() => signOut()}>
-        SignOut
-      </button>
-
-      <h3>{status}</h3>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
