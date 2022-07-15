b00b272b302dbfae11a60ce4b45d604183291b3c
himprover <hwang.improver@gmail.com>
2022-07-15 21:02:52 +0900

    feat: add react-query for server data [MINFOLINK-7]
    1. react-query + SSR 도입 (rehydrate)
    2. 로그인 user 데이터를 SSR 환경에서 react-query로 로드 후 rehydrate
    3. user 데이터의 경우 retry : 0

--- src/components/base/Header/index.tsx
+++ src/components/base/Header/index.tsx
@@ -1,12 +1,32 @@
-import {Session} from 'next-auth';
+import {getUser} from 'core/apis/user';
 import {signIn, signOut} from 'next-auth/react';
+import {useEffect} from 'react';
+import cookie from 'react-cookies';
+import {useQuery} from 'react-query';
 import styled from 'styled-components';
 
-interface HeaderProps {
-  session: Session | null;
-}
+export type UserProps = {
+  id: string;
+  email: string;
+  link: string;
+  nickname: string;
+  profile_image: string;
+  refresh_token: string;
+  is_enabled: boolean;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
+  terms: boolean;
+  privacy: boolean;
+  marketing: boolean;
+};
 
-export const Header = ({session}: HeaderProps) => {
+export const Header = () => {
+  const user = useQuery('user', getUser, {retry: 0});
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
     <Wrap>
       <Nav>
@@ -16,11 +36,7 @@ export const Header = ({session}: HeaderProps) => {
         </NavItemContainer>
         <NavItemContainer>
           <NavItem>
-            {session ? (
-              <button type="button" onClick={() => signOut()}>
-                로그아웃
-              </button>
-            ) : (
+            {user.isError || user.isLoading ? (
               <button
                 type="button"
                 onClick={() =>
@@ -30,6 +46,17 @@ export const Header = ({session}: HeaderProps) => {
                 }
               >
                 로그인/회원가입
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={() => {
+                  signOut();
+                  cookie.remove('accessToken');
+                  cookie.remove('refreshToken');
+                }}
+              >
+                로그아웃
               </button>
             )}
           </NavItem>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
-import type {NextPage} from 'next';
+import {withUser} from 'components/base/Hoc/withUser';
+import type {GetServerSidePropsContext, NextPage} from 'next';
 
 const Home: NextPage = () => {
   return <div>메인</div>;
 };
+
+export const getServerSideProps = withUser(
+  queryClient => async (context: GetServerSidePropsContext) => {
+    return;
+  }
+);
 
 export default Home;
