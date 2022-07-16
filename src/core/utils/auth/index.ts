2a9b04cefca0f6f627f40e4ef16ec657d3db0d93
himprover <hwang.improver@gmail.com>
2022-07-16 15:08:18 +0900

    refactor: delete unuse expires option in cookie [MINFOLINK-7]
    1. 쿠키 저장 시 만료일 설정 삭제

--- src/core/utils/auth/index.ts
+++ src/core/utils/auth/index.ts
@@ -8,17 +8,12 @@ export const setToken = ({
   const HTTP_ONLY: boolean =
     process.env.NODE_ENV === 'development' ? false : true;
 
-  const expires = new Date();
-  expires.setDate(Date.now() + 1000 * 60 * 60 * 24);
-
   cookie.save('accessToken', accessToken, {
     path: '/',
-    expires,
     httpOnly: HTTP_ONLY,
   });
   cookie.save('refreshToken', refreshToken, {
     path: '/',
-    expires,
     httpOnly: HTTP_ONLY,
   });
 };
